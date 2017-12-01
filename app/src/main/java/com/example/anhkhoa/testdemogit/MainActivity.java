ec623b07f7861e8ef4229baba5e78bb555c1fad4
smass77856 <34158047+smass77856@users.noreply.github.com>
2017-12-01 17:21:14 +0700

    Update MainActivity.java

--- app/src/main/java/com/example/anhkhoa/testdemogit/MainActivity.java
+++ app/src/main/java/com/example/anhkhoa/testdemogit/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(this, "Anh Khoaasdasdasdasdas ", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "Anh sdas ", Toast.LENGTH_SHORT).show();
     }
 }
